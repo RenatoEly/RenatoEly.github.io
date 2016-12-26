1e4c6ddc318538c0c7ed80ba0ad520622c62c4f1
RenatoEly <ely.reds@gmail.com>
2016-12-26 11:56:34 +0000

    Corrigindo erro forEach

--- main.js
+++ main.js
@@ -7,51 +7,76 @@ function main(){
        if (error){
            return console.warn(error);
        } 
-       var canvas = d3.select("svg");
-       var dados = [{hora:"0:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"1:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"2:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"3:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"4:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"5:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"6:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"7:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"8:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"9:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"10:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"11:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"12:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"13:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"14:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"15:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"16:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"17:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"18:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"19:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"20:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"21:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"22:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0},
-                    {hora:"23:00", tempoEsquerda:0, tempoDireita:0, qtdEsquerda:0, qtdDireita:0}];
+       var dados = [];
+       var dados[0] = [{hora:"0:00", tempo:0, qtd:0},
+                    {hora:"1:00", tempo:0, qtd:0},
+                    {hora:"2:00", tempo:0, qtd:0},
+                    {hora:"3:00", tempo:0, qtd:0},
+                    {hora:"4:00", tempo:0, qtd:0},
+                    {hora:"5:00", tempo:0, qtd:0},
+                    {hora:"6:00", tempo:0, qtd:0},
+                    {hora:"7:00", tempo:0, qtd:0},
+                    {hora:"8:00", tempo:0, qtd:0},
+                    {hora:"9:00", tempo:0, qtd:0},
+                    {hora:"10:00", tempo:0, qtd:0},
+                    {hora:"11:00", tempo:0, qtd:0},
+                    {hora:"12:00", tempo:0, qtd:0},
+                    {hora:"13:00", tempo:0, qtd:0},
+                    {hora:"14:00", tempo:0, qtd:0},
+                    {hora:"15:00", tempo:0, qtd:0},
+                    {hora:"16:00", tempo:0, qtd:0},
+                    {hora:"17:00", tempo:0, qtd:0},
+                    {hora:"18:00", tempo:0, qtd:0},
+                    {hora:"19:00", tempo:0, qtd:0},
+                    {hora:"20:00", tempo:0, qtd:0},
+                    {hora:"21:00", tempo:0, qtd:0},
+                    {hora:"22:00", tempo:0, qtd:0},
+                    {hora:"23:00", tempo:0, qtd:0},];
+        
+        var dados[1] = [{hora:"0:00", tempo:0, qtd:0},
+                    {hora:"1:00", tempo:0, qtd:0},
+                    {hora:"2:00", tempo:0, qtd:0},
+                    {hora:"3:00", tempo:0, qtd:0},
+                    {hora:"4:00", tempo:0, qtd:0},
+                    {hora:"5:00", tempo:0, qtd:0},
+                    {hora:"6:00", tempo:0, qtd:0},
+                    {hora:"7:00", tempo:0, qtd:0},
+                    {hora:"8:00", tempo:0, qtd:0},
+                    {hora:"9:00", tempo:0, qtd:0},
+                    {hora:"10:00", tempo:0, qtd:0},
+                    {hora:"11:00", tempo:0, qtd:0},
+                    {hora:"12:00", tempo:0, qtd:0},
+                    {hora:"13:00", tempo:0, qtd:0},
+                    {hora:"14:00", tempo:0, qtd:0},
+                    {hora:"15:00", tempo:0, qtd:0},
+                    {hora:"16:00", tempo:0, qtd:0},
+                    {hora:"17:00", tempo:0, qtd:0},
+                    {hora:"18:00", tempo:0, qtd:0},
+                    {hora:"19:00", tempo:0, qtd:0},
+                    {hora:"20:00", tempo:0, qtd:0},
+                    {hora:"21:00", tempo:0, qtd:0},
+                    {hora:"22:00", tempo:0, qtd:0},
+                    {hora:"23:00", tempo:0, qtd:0},];
+                    
        
-       csv.foreach(function(d){
+       csv.forEach(function(d){
            var startDate = new Date(d["Start Date (UTC)"]);
            var endDate = new Date(d["Submit Date (UTC)"]);
                        
             if(leftChar.search(d["agora uma digitação aleatória usando todo o teclado"]) !== -1){
-                dados[startDate.getHours()].qtdEsquerda++;
-                dados[startDate.getHours()].tempoEsquerda += (endDate.getTime() - startDate.getTime())/1000;
+                dados[0][startDate.getHours()].qt++;
+                dados[0][startDate.getHours()].tempo += (endDate.getTime() - startDate.getTime())/1000;
             }
             else if(rightChar.search(d["agora uma digitação aleatória usando todo o teclado"]) !== -1){
-                dados[startDate.getHours()].qtdDireita++;
-                dados[startDate.getHours()].tempoDireita += (endDate.getTime() - startDate.getTime())/1000;
+                dados[0][startDate.getHours()].qtd++;
+                dados[0][startDate.getHours()].tempo += (endDate.getTime() - startDate.getTime())/1000;
             }
        });
        
-       var maxTime = d3.max(dados,function(d){
-                if(d.tempoEsquerda > tempoDireita) {
-                    return d.tempoEsquerda;
-                }
-                return d.tempoDireita;
+       var maxTime = d3.max(dados,function(d,i){
+                return d3.max(d[i],function(d){
+                    return d.tempo/d.qtd;
+                });
            });
            
        var yScale = d3.scale.linear()
